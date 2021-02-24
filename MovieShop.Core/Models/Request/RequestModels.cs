011f0a33b75d3418fcd79eb3c93b0418381df061
Huyen Vu <quyhuyen.vu@gmail.com>
2021-02-23 19:49:52 -0600

    start authentication, added register markup

---
+++ MovieShop.Core/Models/Request/RequestModels.cs
@@ -0,0 +1,32 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieShop.Core.Models.Request
+{
+    public class UserRegisterRequestModel
+    {
+        [Required]
+        [StringLength(50)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long", MinimumLength = 8)]
+        [DataType(DataType.Password)]
+        [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[#$^+=!*()@%&]).{8,}$")]
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long", MinimumLength = 8)]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords must match")]
+        public string RePassword { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+    }
+}
--- MovieShop.MVC/Controllers/AccountController.cs
+++ MovieShop.MVC/Controllers/AccountController.cs
@@ -1,29 +1,20 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
+using MovieShop.Core.Models.Request;
 
 namespace MovieShop.MVC.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Register(UserRegisterRequestModel userModel)
         {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
             return View();
         }
         
-        [HttpGet("/Account/Details/{id}")]
-        public IActionResult Details(int id)
-        {
-            Console.WriteLine(id);
-            return View(id);
-        }
-
-        [HttpPost]
-        public IActionResult Create(Object accountRequest) // Replace with Account request model later
-        {
-            Console.WriteLine(accountRequest);
-            return View();
-        }
-
         [HttpPost]
         public IActionResult Login(Object accountRequest) // Replace with Account request model later
         {
