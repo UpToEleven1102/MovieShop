ab7c32a0d9610bb1e320c7a9fa0b13041ccea396
Huyen Vu <quyhuyen.vu@gmail.com>
2021-03-04 18:20:04 -0600

    added prettier

--- MovieShopSPA/src/app/app-routing.module.ts
+++ MovieShopSPA/src/app/app-routing.module.ts
@@ -1,18 +1,17 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {HomeComponent} from './home/home.component';
-import {MovieListComponent} from './movies/movie-list/movie-list.component';
-import {MovieDetailsComponent} from './movies/movie-details/movie-details.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MovieListComponent } from './movies/movie-list/movie-list.component';
+import { MovieDetailsComponent } from './movies/movie-details/movie-details.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'movies', component: MovieListComponent},
-  {path: 'movies/:id', component: MovieDetailsComponent}
+  { path: '', component: HomeComponent },
+  { path: 'movies', component: MovieListComponent },
+  { path: 'movies/:id', component: MovieDetailsComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
